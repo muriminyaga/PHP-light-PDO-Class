cf7e06993307af4bedb19893b6c11b5b1942f68d
poplax <linjiang9999@gmail.com>
2011-09-23 13:46:10 +0800

    =second commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$config = array();
+$config['Database'] = array();
+$config['Database']['dbtype'] = 'mysql';
+$config['Database']['dbname'] = 'yourdbname';
+$config['Database']['host'] = '127.0.0.1';
+$config['Database']['port'] = 3306;
+$config['Database']['username'] = 'yourusername';
+$config['Database']['password'] = 'yourpassword';
+$config['Database']['charset'] = 'utf8';
+?>
